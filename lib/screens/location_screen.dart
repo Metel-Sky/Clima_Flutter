bf0b31a3df31b934f35c7f87ce22862ef3384b8a
djkif <djkif1212@gmail.com>
2024-09-18 10:14:16 +0300

    Flutter Weather

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,3 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:clima_flutter/utilities/constants.dart';
 
+class LocationScreen extends StatefulWidget{
+  @override
+  _LocationScreenState createState()=> _LocationScreenState();
+
+}
+
+class _LocationScreenState extends State<LocationScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(
+        "WEATHER",
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+        centerTitle: true,
+        backgroundColor: Colors.black,),
+      body: Container(),
+    );
+
+  }
+}
