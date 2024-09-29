9d72cb98f23feddbe622a5b016894e180b01714f
djkif <djkif1212@gmail.com>
2024-09-29 12:19:25 +0300

    Flutter Weather

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,12 +2,9 @@ import 'package:clima_flutter/screens/location_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-import '../services/location.dart';
 import '../services/weather.dart';
 
 const apiKey = 'cb717aa65605c59d6d0dbfc3a8b65fa7';
-const kyiv =
-    'https://api.openweathermap.org/data/2.5/weather?lat=50.4791&lon=30.5932&appid=cb717aa65605c59d6d0dbfc3a8b65fa7';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -17,21 +14,32 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  bool isLoading = false; // Перемикач стану для спінера
 
   @override
   void initState() {
     super.initState();
-    getLocationData();
-    getPermission();
+    // Видалив `getPermission`, якщо це не є необхідним для поточного функціоналу
   }
 
+  // Метод для отримання даних погоди
   void getLocationData() async {
+    setState(() {
+      isLoading = true; // Почати завантаження
+    });
 
     var weatherData = await WeatherModel().getLocationWeather();
-    
-    Navigator.push(context, MaterialPageRoute(builder: (context){
-      return LocationScreen(locationWeather: weatherData,);
+
+    // Після отримання даних, переходимо на наступний екран
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
     }));
+
+    setState(() {
+      isLoading = false; // Закінчити завантаження (необов'язково)
+    });
   }
 
   @override
@@ -46,26 +54,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
         backgroundColor: Colors.black,
       ),
       body: Center(
-        child: SpinKitFadingCircle(
+        child: isLoading
+            ? SpinKitFadingCircle(
           color: Colors.deepPurpleAccent,
           size: 100,
+        )
+            : TextButton(
+          onPressed: () {
+            getLocationData(); // Запит на отримання погоди
+          },
+          child: Text(
+            'Get location',
+            style: TextStyle(fontSize: 40),
+          ),
         ),
-
       ),
     );
   }
 }
-
-
-
-
-
-// child: TextButton(
-//     onPressed: () {
-//       getPermission(); //перевіряє чи є дозвіл на використання геоданних
-//       getLocationData(); // запит на місце положення
-//     },
-//     child: Text(
-//       'Get location',
-//       style: TextStyle(fontSize: 40),
-//     )),
